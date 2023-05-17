4596689ba9794e9e91590402fa355615a8de2f2f
peter-mwau <kyalepeter2000@gmail.com>
2023-05-17 20:54:35 +0300

    Added requirements.txt

--- CodeAssistant/codeassist/views.py
+++ CodeAssistant/codeassist/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from . forms import AssistChat
 
-# Create your views here.
+
+@csrf_exempt
+def assist(request):
+    form = AssistChat(request.POST or None)
+    if form.is_valid():
+        userInput = form.cleaned_data.get("userInput")
+        response = requests.post("https://elitecode-detect-emotions.hf.space/run/predict", json={
+        "data": [
+            userInput,
+        ]
+        }).json()
+        data = response["data"]
+        #decode the json data
+        # convert to string
+        data = json.dumps(data) 
+        # convert to dictionary
+        data = json.loads(data)  
+        output = data[0]['label']
+        formoutput = EmotionForm(initial={'output': output, 'userInput': userInput})
+        # print(formoutput)
+        form = EmotionForm(request.POST or None)
+        r = Records_log(Query=userInput, Emotion=output)
+        r.save()
+        return render(request, 'home.html', {'form': formoutput})
+    return render(request, 'home.html', {'form': form})
