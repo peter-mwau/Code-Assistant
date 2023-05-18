fe6fc5e5a3af26cd324a5a54ad16c0773164629a
peter-mwau <kyalepeter2000@gmail.com>
2023-05-18 18:18:51 +0300

    Updated

--- CodeAssistant/codeassist/views.py
+++ CodeAssistant/codeassist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from . forms import AssistChat
+from gradio_client import Client
 
 
 @csrf_exempt
@@ -8,22 +9,15 @@ def assist(request):
     form = AssistChat(request.POST or None)
     if form.is_valid():
         userInput = form.cleaned_data.get("userInput")
-        response = requests.post("https://elitecode-detect-emotions.hf.space/run/predict", json={
-        "data": [
-            userInput,
-        ]
-        }).json()
-        data = response["data"]
-        #decode the json data
-        # convert to string
-        data = json.dumps(data) 
-        # convert to dictionary
-        data = json.loads(data)  
-        output = data[0]['label']
-        formoutput = EmotionForm(initial={'output': output, 'userInput': userInput})
-        # print(formoutput)
-        form = EmotionForm(request.POST or None)
-        r = Records_log(Query=userInput, Emotion=output)
-        r.save()
+        client = Client("https://mosaicml-mpt-7b-chat--lst2j.hf.space/")
+        result = client.predict(
+				userInput,	# str representing string value in 'Chat Message Box' Textbox component
+				"null",	# str representing filepath to JSON file in 'parameter_3' Chatbot component
+				fn_index=0
+            )
+        print(result)
+        # form = EmotionForm(request.POST or None)
+        # r = Records_log(Query=userInput, Emotion=output)
+        # r.save()
         return render(request, 'home.html', {'form': formoutput})
     return render(request, 'home.html', {'form': form})
